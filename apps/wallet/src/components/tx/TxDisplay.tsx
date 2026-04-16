6d3cb6aa5759ff48519770e1c35f51b7601a3c49
Alireza Mousavi <alir.msvi@gmail.com>
2026-04-16 14:36:12 +0330

    new version

--- apps/wallet/src/components/tx/TxDisplay.tsx
+++ apps/wallet/src/components/tx/TxDisplay.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 
 import * as wasm from '@minotaur-ergo/ergo-lib';
-import { StateWallet } from '@minotaur-ergo/types';
+import { StateWallet, TxStatus } from '@minotaur-ergo/types';
 import { getValueColor } from '@minotaur-ergo/utils';
 import { OpenInNew } from '@mui/icons-material';
-import { IconButton, Typography } from '@mui/material';
+import { Avatar, Box, IconButton, Typography } from '@mui/material';
 
 import { openTxInBrowser } from '@/action/tx';
 import ErgAmount from '@/components/amounts-display/ErgAmount';
+import TransactionResult from '@/components/tx/TransactionResult';
 import useIssuedAndBurntTokens from '@/hooks/useIssuedAndBurntTokens';
+import { useTokensTotalInErg } from '@/hooks/useTokensTotalInErg';
 import useTxValues from '@/hooks/useTxValues';
 import TxAssetDetail from '@/pages/wallet-page/transaction/TxAssetDetail';
 
@@ -24,21 +26,51 @@ const TxDisplay = ({ tx, boxes, wallet, date }: TxDisplayPropsType) => {
   const { mapped } = useIssuedAndBurntTokens(tx, boxes);
   const { txValues } = useTxValues(tx, boxes, wallet);
   const openTx = () => openTxInBrowser(wallet.networkType, txId ?? '');
+  const tokensCount = useMemo(
+    () =>
+      Object.entries(txValues.tokens).filter(([_, balance]) => balance !== 0n)
+        .length,
+    [txValues.tokens],
+  );
+  const tokenList = useMemo(
+    () =>
+      Array.from(Object.entries(txValues.tokens)).map(([tokenId, balance]) => ({
+        tokenId,
+        balance: -balance,
+      })),
+    [txValues.tokens],
+  );
+  const totalTokensInErg = useTokensTotalInErg(tokenList);
   return (
     <React.Fragment>
-      <Typography
-        fontSize="2rem"
-        textAlign="center"
-        color={getValueColor(-txValues.total)}
-        mb={2}
-      >
-        <ErgAmount
-          amount={txValues.total > 0 ? txValues.total : -txValues.total}
-        />
-        <Typography component="span" ml={1}>
-          ERG
+      <Box>
+        <Box display="flex" justifyContent="center">
+          <Avatar
+            src="/ergo.svg"
+            sx={{ width: '48px', height: '48px', borderRadius: '40px' }}
+          />
+        </Box>
+        <Typography
+          fontSize="2rem"
+          textAlign="center"
+          color={getValueColor(-txValues.total)}
+        >
+          <ErgAmount
+            amount={txValues.total > 0 ? txValues.total : -txValues.total}
+          />
+          <Typography component="span" ml={1}>
+            ERG
+          </Typography>
         </Typography>
-      </Typography>
+        <Box display="flex" justifyContent="center" mb={2}>
+          <TransactionResult
+            totalTokensInErg={totalTokensInErg}
+            amount={-txValues.total}
+            txType={TxStatus.IN}
+            withBg={true}
+          />
+        </Box>
+      </Box>
       {date ? (
         <React.Fragment>
           <Typography variant="body2" color="textSecondary">
@@ -58,6 +90,30 @@ const TxDisplay = ({ tx, boxes, wallet, date }: TxDisplayPropsType) => {
           </IconButton>
         </Typography>
       </div>
+      {tokensCount > 0 && (
+        <Typography variant="body2" color="textSecondary">
+          Tokens{' '}
+          <Box
+            component="span"
+            sx={{
+              display: 'inline-flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+              width: '20px',
+              height: '20px',
+              borderRadius: '4px',
+              bgcolor: '#E0E0E0',
+              fontSize: '12px',
+              fontWeight: 400,
+              lineHeight: '16px',
+              letterSpacing: '0.16px',
+              color: 'textSecondary',
+            }}
+          >
+            {tokensCount}
+          </Box>
+        </Typography>
+      )}
       {Object.entries(txValues.tokens).map((item) => (
         <React.Fragment key={item[0]}>
           <TxAssetDetail
